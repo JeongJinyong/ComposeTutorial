7afbaffe13b85be188822e0abe4d122ce65f0c11
skwntnftkgk <skwntnftkgk@gmail.com>
2022-09-04 21:02:22 +0900

    [Feat] Tutorial - Lists and animations

--- app/src/main/java/com/compose/tutorial/MainActivity.kt
+++ app/src/main/java/com/compose/tutorial/MainActivity.kt
@@ -4,14 +4,19 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -24,8 +29,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
+                Surface {
+                    Conversation(messages = SampleData.conversationSample)
                 }
             }
         }
@@ -46,12 +51,35 @@ fun MessageCard(msg: Message) {
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(text = msg.author, color = MaterialTheme.colors.secondaryVariant, style = MaterialTheme.typography.subtitle2)
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
-                Text(text = msg.body, modifier = Modifier.padding(4.dp), style = MaterialTheme.typography.body2)
+            Surface(
+                shape = MaterialTheme.shapes.medium, elevation = 1.dp, color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(4.dp),
+                    style = MaterialTheme.typography.body2,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                )
             }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
         }
     }
 }
@@ -63,12 +91,10 @@ fun MessageCard(msg: Message) {
     name = "Dark Mode"
 )
 @Composable
-fun PreviewMessageCard() {
+fun PreviewConversation() {
     ComposeTutorialTheme {
         Surface {
-            MessageCard(
-                msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-            )
+            Conversation(messages = SampleData.conversationSample)
         }
     }
 }
